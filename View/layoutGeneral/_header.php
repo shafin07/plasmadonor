d381b9ec53a1d438950e6d7699ec1362450d855e
shafin07 <shafinahmad07@gmail.com>
2021-01-02 20:15:17 +0600

    TestCode

---
+++ View/layoutGeneral/_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?= $title; ?></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='http://fonts.googleapis.com/css?family=Lobster|Tangerine|Cantarell:italic|Droid+Serif:bold' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="../assetsGeneral/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assetsGeneral/css/mystyle.css">
+        <link rel="shortcut icon" type="image/png" href="http://localhost/MVC/Views/assetsGeneral/favicon.jpg"/>
+        <script src="../assetsGeneral/js/jquery.min.js"></script>
+        <script src="../assetsGeneral/js/bootstrap.min.js"></script>
+        <script src="../assetsGeneral/js/myjs.js"></script>
+    </head>
+    <body>
+
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'DBConnect.php';
+require_once 'Models/DBConnectGeneral.php';
 $db = new DBConnect();
 $db->authLogin(); // if user has logged in already then forward it to home.php
 
@@ -10,11 +10,50 @@ if(isset($_POST['loginBtn'])){
     
     $flag = $db->login($username, $password);
     if($flag){
-        header("Location: http://localhost/newdonor/MVC_Structure/View/home.php");
+        header("Location: Views/homeGeneral.php");
     } else {
-        $message = "Username of password was incorrect!";
+        $message = "Username of password was incorrect! If you don't have an account please sign up.";
     }
 }
 $title="Login";
-include 'layout/_header.php'; 
+include 'Views/layoutGeneral/_header.php'; 
 ?>
+
+<div class="container">
+<div class="row">
+    <div class="col-md-6">
+        <?php if(isset($message)): ?>
+        <div class="alert-danger"><?= $message; ?></div>
+        <?php endif; ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="col-md-6">
+                    <img src="Views/assetsGeneral/security-icon.png" class="img img-responsive img-thumbnail">
+                </div>
+                <h5> User Login </h5>
+            </div>
+            <div class="panel-body">
+                <form class="form-vertical" role="form" method="post" action="index.php">
+                    <div class="form-group">
+                        <input type="text" class="form-control" required="true" name="username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" required="true" class="form-control" name="password" placeholder="Password">
+                    </div>
+                    <div class="form-group loginBtn">
+                        <button type="submit" name="loginBtn" class="btn btn-primary btn-sm">Login</button>
+                        <p class="text-danger">Don't have a username and Password?</p>
+                        <a class="btn btn-lg btn-danger" href="users/register.php">Signup</a>
+                    </div>
+                </form>
+            </div>
+			</div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <img src="Views/assetsGeneral/left-index-logo.jpg" class="img img-responsive">
+    </div>
+</div>
+
+<?php include 'Views/layoutGeneral/_footer.php'; ?>
+
